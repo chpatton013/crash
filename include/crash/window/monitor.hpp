b92c3a0848d20fa2cc0ca114dc3ea9b7da33e475
Christopher Patton <chpatton013@gmail.com>
2014-12-14 23:29:24 -0800

    Move print functions into Monitor

--- include/crash/window/monitor.hpp
+++ include/crash/window/monitor.hpp
@@ -11,6 +11,20 @@
 namespace crash {
 namespace window {
 
+class Monitor;
+
+void print(const Monitor& monitor);
+
+void print(const Monitor& monitor, bool newline);
+
+void print(const GLFWvidmode* mode);
+
+void print(const GLFWvidmode* mode, bool newline);
+
+void print(const GLFWgammaramp* gamma);
+
+void print(const GLFWgammaramp* gamma, bool newline);
+
 class Monitor : public GlfwAdapter {
 public:
    /////////////////////////////////////////////////////////////////////////////
--- src/window/monitor.cpp
+++ src/window/monitor.cpp
@@ -1,6 +1,78 @@
+#include <cstdio>
+#include <iostream>
 #include <crash/window/monitor.hpp>
 
 using namespace crash::window;
+
+void crash::window::print(const Monitor& monitor) {
+   print(monitor, true);
+}
+
+void crash::window::print(const Monitor& monitor, bool newline) {
+   const char* formatString;
+   if (newline) {
+      formatString = "Monitor; name(%s); position(%d,%d); size(%d,%d)\n";
+   } else {
+      formatString = "Monitor; name(%s); position(%d,%d); size(%d,%d)";
+   }
+   printf(formatString, monitor.name().data(),
+    monitor.position().x, monitor.position().y,
+    monitor.size().x, monitor.size().y);
+}
+
+void crash::window::print(const GLFWvidmode* mode) {
+   print(mode, true);
+}
+
+void crash::window::print(const GLFWvidmode* mode, bool newline) {
+   const char* formatString;
+   if (newline) {
+      formatString = "GLFWvidmode; size(%d,%d); color(%d,%d,%d); refresh(%d)\n";
+   } else {
+      formatString = "GLFWvidmode; size(%d,%d); color(%d,%d,%d); refresh(%d)";
+   }
+   printf(formatString, mode->width, mode->height,
+    mode->redBits, mode->greenBits, mode->blueBits,
+    mode->refreshRate);
+}
+
+void crash::window::print(const GLFWgammaramp* gamma) {
+   print(gamma, true);
+}
+
+void crash::window::print(const GLFWgammaramp* gamma, bool newline) {
+   std::cout << "GLFWgammaramp; size(" << gamma->size << ");";
+
+   if (gamma->size > 0) {
+      std::cout << " red(" << gamma->red[0];
+      if (gamma->size > 1) {
+         for (unsigned int ndx = 1; ndx < gamma->size; ++ndx) {
+            std::cout << "," << gamma->red[ndx];
+         }
+      }
+      std::cout << ");";
+
+      std::cout << " green(" << gamma->green[0];
+      if (gamma->size > 1) {
+         for (unsigned int ndx = 1; ndx < gamma->size; ++ndx) {
+            std::cout << "," << gamma->green[ndx];
+         }
+      }
+      std::cout << ");";
+
+      std::cout << " blue(" << gamma->blue[0];
+      if (gamma->size > 1) {
+         for (unsigned int ndx = 1; ndx < gamma->size; ++ndx) {
+            std::cout << "," << gamma->blue[ndx];
+         }
+      }
+      std::cout << ");";
+   }
+
+   if (newline) {
+      std::cout << std::endl;
+   }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Getters.
